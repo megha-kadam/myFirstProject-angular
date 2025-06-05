eeb666345a2ea53ff01d64a6fa0fdaa26f51177f
megha-kadam <meghakadam506@gmail.com>
2025-06-05 10:08:37 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,4 +25,64 @@ export class AppComponent {
   pharmacy = 'Pharma Topics'
 
   pharmaSub : Array<string> = ['Pharmacognosy', 'Pharmaceutics', 'Pharmacology', 'Human Anatomy', 'Hospital Pharmacy']
+
+  table = "Table"
+
+  stdList = 'Students MarkList';
+
+  studentGrades = [
+	{
+		name:'Jhon',
+		math:85,
+		science:90,
+		english:88,
+	},
+	{
+		name:'Jane',
+		math:75,
+		science:95,
+		english:89,
+	},
+	{
+		name:'Mike',
+		math:78,
+		science:83,
+		english:80,
+	},
+	{
+		name:'Sara',
+		math:95,
+		science:98,
+		english:94,
+	}
+]
+
+book = "Books with Author";
+
+bookCollection = [
+	{
+		booktitle:'1984',
+		author:'George Orwell',
+		genre:'Dystopian',
+		year:1949,
+	},
+	{
+		booktitle:'To kill a Mockingbird',
+		author:'George Orwell',
+		genre:'Harper Lee',
+		year:1960,
+	},
+	{
+		booktitle:'The Great Gatsby',
+		author:'F. Scott Fitzgerald',
+		genre:'Fiction',
+		year:1925,
+	},
+	{
+		booktitle:'Moby Dick',
+		author:'Herman Melville',
+		genre:'Adventure',
+		year:1851,
+	},
+]
 }
